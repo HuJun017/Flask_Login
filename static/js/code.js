c707f234ce8f5ddf82f2c91e4769ead61af36cac
Jun Hu <hu.jun@iisgalvanimi.edu.it>
2024-05-03 15:52:54 +0000

    updated files

--- static/js/code.js
+++ static/js/code.js
@@ -5,14 +5,28 @@ function getElenco() {
     let passwordInput = document.getElementById('passwordInput').value;
     let trovato = false;
     let i = 0; //indice
-
+    
     //estrazione dati
     fetch('https://3245-hujun017-flasklogin-iwzllonrxe7.ws-eu110.gitpod.io/elenco')
     .then(response => response.json())
     .then(data => {
         //visualizzazione dei dati
         console.log(data);
-        console.log(data.length)
+        //trovare il password
         //while (trovato === false && i < data.length)
+        let lunghezzaData = Object.keys(data.username).length;
+        while (i < lunghezzaData && !trovato) {
+            if (data.password[i] === passwordInput && data.username[i] === usernameInput && data.account[i] === accountInput) {
+                trovato = true;
+            }
+            i++;
+        }
+        //check per vedere se il password è stato trovato
+        if (trovato) {
+            console.log("La password è stata trovata!");
+            window.location.href = "https://3245-hujun017-flasklogin-iwzllonrxe7.ws-eu110.gitpod.io/test"
+        } else {
+            console.log("La password non è stata trovata.");
+        }
     })
 }
