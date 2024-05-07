0da2c39a4b6cfa3584958e20a8a84a77815d68dd
Jun Hu <hu.jun@iisgalvanimi.edu.it>
2024-05-07 07:08:35 +0000

    updated

--- static/js/code.js
+++ static/js/code.js
@@ -1,32 +1,21 @@
+let usernameInput = document.getElementById('usernameInput').value;
+let accountInput = document.getElementById('accountInput').value;
+let passwordInput = document.getElementById('passwordInput').value;
+let user = {
+    username: usernameInput,
+    account: accountInput,
+    password: passwordInput
+};
+
 function getElenco() {
-    //dichiarazioni variabili
-    let usernameInput = document.getElementById('usernameInput').value;
-    let accountInput = document.getElementById('accountInput').value;
-    let passwordInput = document.getElementById('passwordInput').value;
-    let trovato = false;
-    let i = 0; //indice
-    
-    //estrazione dati
-    fetch('https://3245-hujun017-flasklogin-iwzllonrxe7.ws-eu111.gitpod.io/elenco')
-    .then(response => response.json())
-    .then(data => {
-        //visualizzazione dei dati
-        console.log(data);
-        //trovare il password
-        //while (trovato === false && i < data.length)
-        let lunghezzaData = Object.keys(data.username).length;
-        while (i < lunghezzaData && !trovato) {
-            if (data.password[i] === passwordInput && data.username[i] === usernameInput && data.account[i] === accountInput) {
-                trovato = true;
-            }
-            i++;
-        }
-        //check per vedere se il password è stato trovato
-        if (trovato) {
-            console.log("La password è stata trovata!");
-            window.location.href = "https://3245-hujun017-flasklogin-iwzllonrxe7.ws-eu111.gitpod.io/test"
-        } else {
-            console.log("La password non è stata trovata.");
-        }
+    fetch('https://3245-hujun017-flasklogin-iwzllonrxe7.ws-eu113.gitpod.io/elenco'), {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json' // Specifica il tipo di contenuto come JSON
+        },
+        body: JSON.stringify(user) // Trasforma i dati in formato JSON
+    }
+    .then(data =>{
+        
     })
 }
