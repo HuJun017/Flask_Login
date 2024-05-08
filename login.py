9f15cf024fcd5241a1653611e075f6a139834b75
Jun Hu <hu.jun@iisgalvanimi.edu.it>
2024-05-08 06:03:20 +0000

    reworked some files

--- login.py
+++ login.py
@@ -1,20 +1,35 @@
-from flask import Flask, render_template, request, jsonify
+import pandas as pd
+from flask import Flask, request, jsonify, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return render_template("home.html")
-
-@app.route('/elenco')
-def information():
-    import pandas as pd
-    df = pd.read_csv("/workspace/Flask_Login/data/users.csv")
-    info = df.username
-    return render_template('json.html', tabella = df.to_json())
+    return render_template('home.html')
 
 @app.route('/test')
 def test_page():
     return render_template('test_page.html')
     
+@app.route('/elenco', methods=['POST'])
+def post_information():
+    df = pd.read_csv('/workspace/Flask_Login/data/users.csv')
+
+    data = request.json
+    if not data:
+        return jsonify({'error': 'I dati non sono stati inviati correttamente'}), 400
+
+    username = data.get('username')
+    account = data.get('account')
+    check = False
+
+    # ciclo per trovare l'utente
+    users_dict = df.to_dict(orient='records')
+    for user in users_dict:
+        if user['username'] == username and user['account'] == account:
+            check = True
+
+    return jsonify(users_dict, check), 200
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
--- static/js/code.js
+++ static/js/code.js
@@ -1,21 +1,32 @@
-let usernameInput = document.getElementById('usernameInput').value;
-let accountInput = document.getElementById('accountInput').value;
-let passwordInput = document.getElementById('passwordInput').value;
-let user = {
-    username: usernameInput,
-    account: accountInput,
-    password: passwordInput
-};
-
 function getElenco() {
-    fetch('https://3245-hujun017-flasklogin-iwzllonrxe7.ws-eu113.gitpod.io/elenco'), {
+    let usernameInput = document.getElementById('usernameInput').value;
+    let accountInput = document.getElementById('accountInput').value;
+    let passwordInput = document.getElementById('passwordInput').value;
+    let user = {
+        username: usernameInput,
+        account: accountInput,
+        password: passwordInput
+    };
+
+    fetch('https://3245-hujun017-flasklogin-iwzllonrxe7.ws-eu111.gitpod.io/elenco', {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json' // Specifica il tipo di contenuto come JSON
+          'Content-Type': 'application/json'
         },
-        body: JSON.stringify(user) // Trasforma i dati in formato JSON
-    }
-    .then(data =>{
-        
+        body: JSON.stringify(user)
     })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        console.log(data[1]);
+        if (data[1] === true) {
+            window.location.href = 'https://3245-hujun017-flasklogin-iwzllonrxe7.ws-eu111.gitpod.io/test';
+        }
+        else {
+            alert('utente inesistente, controlla di aver inserito i dati correttamente!')
+        }
+    })
+    .catch(error => {
+        console.error('Si è verificato un errore:', error);
+    });
 }
